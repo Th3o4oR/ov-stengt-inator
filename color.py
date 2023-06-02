eaa1a5aa91a150053dc4687eaecefe91e7582687
Theodor Johansson <theodor.johansson@propulsentnu.no>
2023-06-02 22:25:44 +0200

    Almost working globals file

--- color.py
+++ color.py
@@ -3,6 +3,8 @@ import time
 import math
 import _thread
 
+import globals
+
 PWM_DUTY_MAX: int = 2**16-1
 PWM_FREQ: int = 100000
 
@@ -18,17 +20,12 @@ RGB_PWM_RED.freq(PWM_FREQ)
 RGB_PWM_GREEN.freq(PWM_FREQ)
 RGB_PWM_BLUE.freq(PWM_FREQ)
 
-def init():    
-    global color_thread_exit
-
-    color_thread_exit = False
-
 class Color:
     def __init__(self, r: float, g: float, b: float, name: str):
-        self.red: float = r
+        self.red:   float = r
         self.green: float = g
-        self.blue: float = b
-        self.name: str = name
+        self.blue:  float = b
+        self.name:  str   = name
 
     def __add__(self, rhs):
         color: Color = Color(
@@ -76,106 +73,108 @@ class Color:
     def __rtruediv__(self, lhs):
         return self.__truediv__(lhs)
 
-RED:    Color = Color(1, 0,    0, "red")
-GREEN:  Color = Color(0, 1,    0, "green")
-BLUE:   Color = Color(0, 0,    1, "blue")
-YELLOW: Color = Color(1, 0.75, 0, "yellow")
-PURPLE: Color = Color(1, 0,    1, "purple")
-BLACK:  Color = Color(0, 0,    0, "black")
-
-global_colors: dict[str, Color] = {
-    "red":    RED,
-    "green":  GREEN,
-    "blue":   BLUE,
-    "yellow": YELLOW,
-    "purple": PURPLE,
-    "black":  BLACK
-}
-
-global_display_color: Color = BLACK
-global_display_color_prev: Color = BLACK
-global_color: Color = BLACK
-global_color_prev: Color = BLACK
-
-global_blink_freq: float = 0
-global_fade_time: float = 1
-global_change_time: int = time.ticks_cpu()
-global_brightness: float = 0.5
-color_thread_exit: bool = False
-
 # Update RGB LED
 def set_rgb(c: Color) -> None:
-    c *= global_brightness
+    c *= globals.BRIGHTNESS
     RGB_PWM_RED.duty_u16(int((1-c.red)*PWM_DUTY_MAX))
     RGB_PWM_GREEN.duty_u16(int((1-c.green)*PWM_DUTY_MAX))
     RGB_PWM_BLUE.duty_u16(int((1-c.blue)*PWM_DUTY_MAX))
 
 # Update color variables
 def change_color(c: Color, blink_freq: float = 0, fade_time: float | None = None) -> None:
-    global global_color, global_color_prev
-    global global_display_color, global_display_color_prev
-    global global_blink_freq, global_fade_time, global_change_time
+    # Update previous and current colors
+    globals.ACTUAL_COLOR_PREV = globals.ACTUAL_COLOR_CURR
+    globals.ACTUAL_COLOR_CURR = c
 
-    global_color_prev  = global_color
-    global_color       = c
+    # Update previous and current display colors
+    globals.DISPLAY_COLOR_PREV = globals.DISPLAY_COLOR_CURR
+    globals.DISPLAY_COLOR_CURR = c
 
-    global_display_color = c
-    global_display_color_prev = global_color_prev
+    # Set the time of change
+    globals.COLOR_CHANGE_TIME = time.ticks_cpu()
 
-    global_change_time = time.ticks_cpu()
-
-    global_blink_freq = blink_freq
+    # Update blink frequency
+    globals.BLINK_FREQUENCY = blink_freq
     if (fade_time != None):
-        global_fade_time  = fade_time
+        globals.FADE_DURATION = fade_time
 
+# Initialize variables required for color thread (and other threads relying on color thread)
+def color_thread_init() -> None:
+    # print("Initializing color thread...")
+
+    # Initialize colors
+    globals.COLOR_MAPPING = {
+        "red":    Color(1, 0,    0, "red"),
+        "green":  Color(0, 1,    0, "green"),
+        "blue":   Color(0, 0,    1, "blue"),
+        "yellow": Color(1, 0.75, 0, "yellow"),
+        "purple": Color(1, 0,    1, "purple"),
+        "black":  Color(0, 0,    0, "black")
+    }
+    globals.DISPLAY_COLOR_CURR = globals.COLOR_MAPPING["black"]
+    globals.DISPLAY_COLOR_PREV = globals.COLOR_MAPPING["black"]
+    globals.ACTUAL_COLOR_CURR  = globals.COLOR_MAPPING["black"]
+    globals.ACTUAL_COLOR_PREV  = globals.COLOR_MAPPING["black"]
+    globals.COLOR_CHANGE_TIME  = time.ticks_cpu()
+
+    # Initialize thread variables
+    globals.COLOR_THREAD_EXIT = False
+
+    # The following variables are overridden in main.py, as they are read from the config file
+    globals.BLINK_FREQUENCY = 0
+    globals.FADE_DURATION   = 1
+    globals.BRIGHTNESS      = 0.5
+
+# Function to be run as color thread
 def color_thread() -> None:
-    global global_color, global_color_prev
-    global global_display_color, global_display_color_prev
-    global global_blink_freq, global_fade_time, color_thread_exit, global_change_time
-
+    # Start thread
     fade_factor: float = 0
     new_blink_value: bool = True
     old_blink_value: bool = True
-    while not color_thread_exit:
+    while not globals.COLOR_THREAD_EXIT:
+        # Get current time and time since last color change (in microseconds)
         t: float = float(time.ticks_cpu())
-        dt: float = time.ticks_diff(int(t), global_change_time)
+        dt: float = time.ticks_diff(int(t), globals.COLOR_CHANGE_TIME)
         
         # Rescale time to seconds
         t = pow(10, -6) * t
         dt = pow(10, -6) * dt
         
+        # Values to check if blink has occured
         old_blink_value = new_blink_value
-        new_blink_value = bool((math.floor(t*2*global_blink_freq)+1)%2) # Initialized to True
+        new_blink_value = bool((math.floor(2*t*globals.BLINK_FREQUENCY)+1)%2)
         
-        if (new_blink_value != old_blink_value and global_blink_freq != 0):
-            global_change_time = time.ticks_cpu()
-            global_display_color_prev = global_display_color
-            global_display_color = [BLACK, global_color][new_blink_value]
+        # Blink when blink should occur
+        if (new_blink_value != old_blink_value and globals.BLINK_FREQUENCY != 0):
+            globals.COLOR_CHANGE_TIME = time.ticks_cpu()
+            globals.DISPLAY_COLOR_PREV = globals.DISPLAY_COLOR_CURR
+            globals.DISPLAY_COLOR_CURR = [globals.COLOR_MAPPING["black"], globals.ACTUAL_COLOR_CURR][new_blink_value] # Indexing magic
 
-        if (global_fade_time == 0):
-            set_rgb(global_display_color * new_blink_value)
+        if (globals.FADE_DURATION == 0):
+            set_rgb(globals.DISPLAY_COLOR_CURR * new_blink_value)
         else:
-            fade_factor = -math.cos(min(dt/global_fade_time, 1)*math.pi)/2 + 0.5
-            new_color: Color = global_display_color_prev*(1-fade_factor) + global_display_color*fade_factor
+            fade_factor = -math.cos(min(dt/globals.FADE_DURATION, 1)*math.pi)/2 + 0.5
+            new_color: Color = globals.DISPLAY_COLOR_PREV*(1-fade_factor) + globals.DISPLAY_COLOR_CURR*fade_factor
             set_rgb(new_color)
 
     print("[Color thread] Terminating...")
-    set_rgb(BLACK)
+    set_rgb(globals.COLOR_MAPPING["black"])
 
 if __name__ == "__main__":
     try:
-        init()
-
+        color_thread_init()
         _thread.start_new_thread(color_thread, ())
+        # time.sleep(0.1)
+        print(globals.COLOR_MAPPING)
 
-        change_color(RED, fade_time=0.5)
+        change_color(globals.COLOR_MAPPING["red"], fade_time=0.5)
         time.sleep(0.5)
-        change_color(BLACK, fade_time=0.5)
+        change_color(globals.COLOR_MAPPING["black"], fade_time=0.5)
         time.sleep(0.5)
-        change_color(BLUE, fade_time=0.5, blink_freq=1)
+        change_color(globals.COLOR_MAPPING["blue"], fade_time=0.5, blink_freq=1)
         time.sleep(10)
         
-        color_thread_exit = True
+        globals.COLOR_THREAD_EXIT = True
     except KeyboardInterrupt:
-        color_thread_exit = True
+        globals.COLOR_THREAD_EXIT = True
 
--- main_file.py
+++ main_file.py
@@ -7,76 +7,54 @@ import ujson
 
 # Support files
 import color
-
-# Global variables
-NETWORKS: dict
-PING_URL: str
-API_URL: str
-API_INTERVAL: int
-CONFIG_NETWORK_NAMES: list[str]
-wlan: network.WLAN
-
-FADE_DURATION: float
-BLINK_FREQUENCY: float
-
-COLOR_MAPPING: dict[str, str]
-COLOR_DOOR_OPEN: color.Color
-COLOR_DOOR_CLOSED: color.Color
-COLOR_API_FAIL: color.Color
-COLOR_ATTEMPTING_CONNECTION: color.Color
-COLOR_CHECKING_INTERNET: color.Color
-
-ONBOARD_LED_PIN: machine.Pin
+import globals
 
 # Initialization
 def init():
-    global NETWORKS, PING_URL, API_URL, API_INTERVAL, CONFIG_NETWORK_NAMES, wlan
-    global FADE_DURATION, BLINK_FREQUENCY
-    global COLOR_MAPPING, COLOR_DOOR_OPEN, COLOR_DOOR_CLOSED, COLOR_API_FAIL, COLOR_ATTEMPTING_CONNECTION, COLOR_CHECKING_INTERNET
-    global ONBOARD_LED_PIN
-
     # Define all pins
-    ONBOARD_LED_PIN: machine.Pin = machine.Pin("LED", machine.Pin.OUT)
+    globals.ONBOARD_LED_PIN = machine.Pin("LED", machine.Pin.OUT)
 
     # Load config
     with (open("config.json", "rb")) as f:
         config: dict = ujson.loads(f.read())
 
-        NETWORKS = config["networks"]
-        CONFIG_NETWORK_NAMES = [NETWORKS[item]["ssid"] for item in sorted(NETWORKS.keys())]
+        # Network stuff
+        globals.NETWORKS = config["networks"]
+        globals.NETWORK_NAMES = [globals.NETWORKS[item]["ssid"] for item in sorted(globals.NETWORKS.keys())]
+        globals.PING_URL = config["ping_url"]
         
-        PING_URL = config["ping_url"]
-        API_URL  = config["api_url"]
-        API_INTERVAL = config["api_interval"]
+        # API stuff
+        globals.API_URL  = config["api_url"]
+        globals.API_INTERVAL = config["api_interval"]
 
-        FADE_DURATION = float(config["color_fade_duration"])
-        BLINK_FREQUENCY = float(min(config["color_blink_frequency"], FADE_DURATION/2))
-
-        COLOR_MAPPING = config["color_mapping"]
+        # Color stuff
+        globals.FADE_DURATION   = float(config["color_fade_duration"])
+        globals.BLINK_FREQUENCY = float(min(config["color_blink_frequency"], globals.FADE_DURATION/2))
+        globals.BRIGHTNESS      = float(config["color_brightness"])
 
     # Colors
-    COLOR_DOOR_OPEN             = color.global_colors[COLOR_MAPPING["door_open"]]
-    COLOR_DOOR_CLOSED           = color.global_colors[COLOR_MAPPING["door_closed"]]
-    COLOR_API_FAIL              = color.global_colors[COLOR_MAPPING["api_fail"]]
-    COLOR_ATTEMPTING_CONNECTION = color.global_colors[COLOR_MAPPING["attempting_connection"]]
-    COLOR_CHECKING_INTERNET     = color.global_colors[COLOR_MAPPING["checking_internet"]]
+    globals.COLOR_DOOR_OPEN       = globals.COLOR_MAPPING[config["color_mapping"]["door_open"]]
+    globals.COLOR_DOOR_CLOSED     = globals.COLOR_MAPPING[config["color_mapping"]["door_closed"]]
+    globals.COLOR_API_FAIL        = globals.COLOR_MAPPING[config["color_mapping"]["api_fail"]]
+    globals.COLOR_WLAN_CONNECTING = globals.COLOR_MAPPING[config["color_mapping"]["wlan_connecting"]]
+    globals.COLOR_PINGING         = globals.COLOR_MAPPING[config["color_mapping"]["pinging"]]
 
     # Connect WLAN
-    color.change_color(COLOR_ATTEMPTING_CONNECTION, fade_time=FADE_DURATION)
-    wlan = network.WLAN(network.STA_IF)
-    wlan.active(True)
+    color.change_color(globals.COLOR_WLAN_CONNECTING, fade_time=globals.FADE_DURATION)
+    globals.WLAN_OBJECT = network.WLAN(network.STA_IF)
+    globals.WLAN_OBJECT.active(True)
     internet_connected: bool = connect_wlan()
     while not internet_connected:
         print("\nX Failed to connect to internet")
         wait(3, string="before retrying")
         print("")
-        color.change_color(COLOR_ATTEMPTING_CONNECTION, fade_time=FADE_DURATION, blink_freq=BLINK_FREQUENCY)
+        color.change_color(globals.COLOR_WLAN_CONNECTING, fade_time=globals.FADE_DURATION, blink_freq=globals.BLINK_FREQUENCY)
         internet_connected = connect_wlan()
     print("")
 
 def get_network_priority_from_name(network_name: str) -> int:
-    for item in sorted(NETWORKS.keys()):
-        if (NETWORKS[item]["ssid"] == network_name):
+    for item in sorted(globals.NETWORKS.keys()):
+        if (globals.NETWORKS[item]["ssid"] == network_name):
             return item
     return -1
 
@@ -105,7 +83,7 @@ def retrieve_url(url: str) -> urequests.Response | None:
 def connect_wlan() -> bool:
     # Get a list of all available networks
     print("Scanning for available networks...")
-    available_networks: list[tuple[bytes, bytes, int, int, int, int]] = wlan.scan()
+    available_networks: list[tuple[bytes, bytes, int, int, int, int]] = globals.WLAN_OBJECT.scan()
     if (len(available_networks) == 0):
         print("X No networks found")
         return False
@@ -114,8 +92,8 @@ def connect_wlan() -> bool:
     union: list[int] = []
     for item in available_networks:
         network_name: str = item[0].decode("utf-8")
-        if (network_name in CONFIG_NETWORK_NAMES):
-            print(f"- Scan revealed network: {network_name}, which exists in config")
+        if (network_name in globals.NETWORK_NAMES):
+            print(f"- Scan revealed known network: {network_name}")
             priority = get_network_priority_from_name(network_name)
             if (priority != -1):
                 union.append(priority)
@@ -123,30 +101,30 @@ def connect_wlan() -> bool:
     # Loop through the available networks in the config, sorted by priority
     for item in sorted(union):
         # Connect to network
-        network_name:     str = NETWORKS[item]["ssid"]
-        network_password: str = NETWORKS[item]["password"]
-        wlan_timeout:     int = NETWORKS[item]["wlan_timeout"]
-        wlan.connect(network_name, network_password)
+        network_name:     str = globals.NETWORKS[item]["ssid"]
+        network_password: str = globals.NETWORKS[item]["password"]
+        wlan_timeout:     int = globals.NETWORKS[item]["wlan_timeout"]
+        globals.WLAN_OBJECT.connect(network_name, network_password)
 
         # Wait for connection
         print(f"Attempting connection to network \"{network_name}\"...")
-        timed_out: bool = not wait(wlan_timeout, exit_condition=lambda: wlan.isconnected(), string="for connection") # Returns true if exit condition met
+        timed_out: bool = not wait(wlan_timeout, exit_condition=lambda: globals.WLAN_OBJECT.isconnected(), string="for connection") # Returns true if exit condition met
         if (timed_out):
             print("X Connection timed out")
             continue
         clear_terminal_line()
-        print("√ Connected to network:", wlan.ifconfig()[0])
+        print("√ Connected to network:", globals.WLAN_OBJECT.ifconfig()[0])
 
         # Attempt login if not connected to internet and login portal URL and body provided
         connected: bool = test_connectivity()
         if (not connected):
             print("X Not connected to internet")
-            if ("login_portal_url" not in NETWORKS[item] or "login_portal_body" not in NETWORKS[item]):
+            if ("login_portal_url" not in globals.NETWORKS[item] or "login_portal_body" not in globals.NETWORKS[item]):
                 print("X No login portal URL or body provided")
                 return False
 
-            login_portal_url  = NETWORKS[item]["login_portal_url"]
-            login_portal_body = NETWORKS[item]["login_portal_body"]
+            login_portal_url  = globals.NETWORKS[item]["login_portal_url"]
+            login_portal_body = globals.NETWORKS[item]["login_portal_body"]
 
             print("Attempting \"login\"...")
             res = urequests.post(login_portal_url, data=login_portal_body)
@@ -164,9 +142,9 @@ def connect_wlan() -> bool:
 # Test if the router has internet connection
 def test_connectivity() -> bool:
     print("\nChecking internet connection...")
-    color.change_color(COLOR_CHECKING_INTERNET, fade_time=FADE_DURATION)
+    color.change_color(globals.COLOR_PINGING, fade_time=globals.FADE_DURATION)
 
-    response: urequests.Response | None = retrieve_url(PING_URL)
+    response: urequests.Response | None = retrieve_url(globals.PING_URL)
     if (response == None):
         return False
     return True
@@ -203,21 +181,21 @@ def __main__():
     init()
 
     # Set onboard LED to on
-    ONBOARD_LED_PIN.value(1)
+    globals.ONBOARD_LED_PIN.value(1)
 
     # Loop
     while True:
         # response = retrieve_url(API_URL)
         print("Calling API...")
-        response: urequests.Response | None = retrieve_url(API_URL)
+        response: urequests.Response | None = retrieve_url(globals.API_URL)
 
         if (response == None):
             print("X API call failed")
 
-            color.change_color(COLOR_API_FAIL, fade_time=FADE_DURATION, blink_freq=BLINK_FREQUENCY)
+            color.change_color(globals.COLOR_API_FAIL, fade_time=globals.FADE_DURATION, blink_freq=globals.BLINK_FREQUENCY)
             if (not test_connectivity()):
                 print("X Lost connection to network, attempting to reconnect...")
-                color.change_color(COLOR_ATTEMPTING_CONNECTION, fade_time=FADE_DURATION, blink_freq=BLINK_FREQUENCY)
+                color.change_color(globals.COLOR_WLAN_CONNECTING, fade_time=globals.FADE_DURATION, blink_freq=globals.BLINK_FREQUENCY)
                 network_connected: bool = connect_wlan()
                 if (network_connected):
                     print("√ Reconnected to network")
@@ -235,10 +213,10 @@ def __main__():
                     raise Exception("Response JSON is None")
                 
                 if (response_json["open"] == "1"):
-                    color.change_color(COLOR_DOOR_OPEN, fade_time=FADE_DURATION)
+                    color.change_color(globals.COLOR_DOOR_OPEN, fade_time=globals.FADE_DURATION)
                     print("Door OPEN", end="")
                 else:
-                    color.change_color(COLOR_DOOR_CLOSED, fade_time=FADE_DURATION)
+                    color.change_color(globals.COLOR_DOOR_CLOSED, fade_time=globals.FADE_DURATION)
                     print("Door CLOSED", end="")
                 print(" for " + str(response_json["time"]) + " seconds\n")
 
@@ -248,15 +226,17 @@ def __main__():
                 # continue
         
         # Wait until next api call
-        wait(API_INTERVAL, string="until next API call")
+        wait(globals.API_INTERVAL, string="until next API call")
 
 try:
-    color.init()
+    color.color_thread_init() # Needs to be called before starting thread, to ensure that the thread is not started before all the required variables have been defined
+    print(globals.BLINK_FREQUENCY)
     _thread.start_new_thread(color.color_thread, ())
+    print(globals.BLINK_FREQUENCY)
     __main__()
+    print(globals.BLINK_FREQUENCY)
 except KeyboardInterrupt:
     print("\nKeyboard interrupt, exiting...")
     clear_terminal_line()
-    color.color_thread_exit = True
-    # time.sleep(0.1)
-    ONBOARD_LED_PIN.value(0)
+    globals.COLOR_THREAD_EXIT = True
+    globals.ONBOARD_LED_PIN.value(0)
